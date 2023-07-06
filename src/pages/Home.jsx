79ce637db69652114d8beb375bf11a16e4529ea1
Ing140943 <skladeeing@gmail.com>
2023-07-06 11:52:24 +0700

    Edit posts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,11 +40,11 @@ const Home = () => {
       if (posts.length === 0) {
         return <div>No posts found.</div>;
       }
-      
+
     return (
         <div className='home'>
             <div className='posts'>
-                {posts.map((post) => (
+                {posts?.map((post) => (
                     <div className='post' key={post.id}>
                         <div className='img'>
                             <img src={post.img} alt='content-img' />
